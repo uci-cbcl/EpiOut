fa563608ac65c2dea27e14e982c3a1225176aa99
mh <mh@mhs-Mac-mini.local>
2024-03-04 18:12:00 -0800

    debug

--- tests/test_main.py
+++ tests/test_main.py
@@ -95,6 +95,12 @@ def test_cli_epiout(tmp_path, counts):
         '--confounders', str(confounders_path),
         '--output_prefix', prefix,
     ])
+    print('exception')
+    print(result.exception)
+    print('stderr')
+    print(result.stderr)
+    print('stdout')
+    print(result.stdout)
     assert result.exit_code == 0
 
     adata = anndata.read_h5ad(prefix + '.h5ad')
