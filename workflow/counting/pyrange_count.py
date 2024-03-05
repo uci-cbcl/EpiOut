d9d27bc2ca14ea0150cd118ba66299542a169a99
M. Hasan Celik <muhammedhasancelik@gmail.com>
2024-03-05 00:08:05 +0000

    model update

--- workflow/counting/pyrange_count.py
+++ workflow/counting/pyrange_count.py
@@ -3,10 +3,7 @@ import pyranges as pr
 
 gr_bed = pr.read_bed(snakemake.input['bed']) \
     .drop().merge(strand=False, slack=200)
-
 gr_bam = pr.read_bam(snakemake.input['bam'], mapq=10)
 
 gr = gr_bed.count_overlaps(gr_bam, overlap_col='count')
-
-gr.drop(drop=['Name', 'Score', 'Strand']) \
-  .to_bed(snakemake.output['counts'])
+gr.to_bed(snakemake.output['counts'])
